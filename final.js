2cd123b4ab976347ba0dd57f84a167c94a578fff
mkidushim <mkidushim@gmail.com>
2015-06-14 14:33:20 -0700

    working on #2 #4 added gitignore

--- final.js
+++ final.js
@@ -1,85 +1,165 @@
 function initialize() {
-	//  var pos = navigator.geolocation.getCurrentPosition();
-	// var lat = position.coords.latitude;
-	// var longitude = position.coords.longitude;
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(lat, longitude),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-                                mapOptions);
+    var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(33, -117),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
 }
-function get_location(){
+
+function get_location() {
 
 
-	navigator.geolocation.getCurrentPosition(function(position) {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-                                mapOptions);
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var mapOptions = {
+            zoom: 12,
+            center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
 
 
-});
+    });
 }
 
 function ajax_call() {
-	$.ajax({
-			url: 'index.php',
-			data: {
-				username: $('#username').val(),
-				password: $('#password').val()
-			},
-			method: 'POST',
-			dataType: 'JSON',
-			success: function(response){
-				console.log(response);
-				window.php_response= response;
-				if(response){
-					console.log('result is true',response)
-					console.log(response)
-					
-					to_landing();
-				}
-				else {
-					console.log('error: ', response)
-					$('#main_content').html('error: '+ response);
-					
-				}
-				
-			}
-		});
-}
-function to_landing (){
-$.ajax({
-			url: 'landing.html',
-			method: 'POST',
-			dataType: 'html',
-			success: function(response){
-				if (response){
-				console.log(response);
-				var user = $('<h3>',{
-					text: 'username:'+php_response['username'],
-					class: 'col-md-7'
-				})
-				$('.main_content').html(user).append(response);
-				initialize();	
-				}
-				else{
-					console.log('error no page');
+    $.ajax({
+        url: 'index.php',
+        data: {
+            username: $('#username').val(),
+            password: $('#password').val()
+        },
+        method: 'POST',
+        dataType: 'JSON',
+        success: function(response) {
+            console.log(response);
+            window.php_response = response;
+            if (response) {
+                console.log('result is true', response)
+                console.log(response)
 
-				}
-				
-			}
-		});
+                to_landing();
+            } else {
+                console.log('error: ', response)
+                $('#main_content').html('error: ' + response);
 
+            }
+
+        }
+    });
 }
-$(document).ready(function(){
-	$('form').on('click', '#login', function(){
-		ajax_call();
-		console.log('button worked')
-	})
+
+function to_landing() {
+    $.ajax({
+        url: 'landing.html',
+        method: 'POST',
+        dataType: 'html',
+        success: function(response) {
+            if (response) {
+                console.log(response);
+                var user = $('<h3>', {
+                    text: php_response['username'],
+                    class: 'col-md-4 col-md-offset-3'
+                })
+                $('.main_content').html(user).append(response);
+                initialize();
+                $('nav').on('click', 'home', function() {
+                    nav_home();
+                })
+                $('nav').on('click', '.lunch', function() {
+                    nav_lunch();
+                })
+                $('nav').on('click', '.friends', function() {
+                    nav_friends();
+                })
+            } else {
+                console.log('error no page');
+
+            }
+
+        }
+    });
+}
+
+function nav_home() {
+    $.ajax({
+        url: 'landing.html',
+        method: 'POST',
+        dataType: 'html',
+        success: function(response) {
+            to_landing();
+            console.log('home');
+            $('nav').on('click', '.home', function() {
+                nav_home();
+            })
+            $('nav').on('click', '.lunch', function() {
+                nav_lunch();
+            })
+            $('nav').on('click', '.friends', function() {
+                nav_friends();
+            })
+
+        }
+    });
+}
+
+function nav_friends() {
+    $.ajax({
+        url: 'friends.html',
+        method: 'POST',
+        dataType: 'html',
+        success: function(response) {
+            $('.main_content').html(response);
+            console.log('friends', response);
+            $('nav').on('click', '.home', function() {
+                nav_home();
+            })
+            $('nav').on('click', '.lunch', function() {
+                nav_lunch();
+            })
+            $('nav').on('click', '.friends', function() {
+                nav_friends();
+            })
+
+        }
+    });
+}
+
+function nav_lunch() {
+    $.ajax({
+        url: 'lunch.html',
+        method: 'POST',
+        dataType: 'html',
+        success: function(response) {
+            $('.main_content').html(response);
+            $('nav').on('click', '.home', function() {
+                nav_home();
+            })
+            $('nav').on('click', '.lunch', function() {
+                nav_lunch();
+            })
+            $('nav').on('click', '.friends', function() {
+                nav_friends();
+            })
+
+        }
+    });
+}
+$(document).ready(function() {
+    $('form').on('click', '#login', function() {
+        ajax_call();
+        console.log('button worked')
+    })
+    $('nav').on('click', '.home', function() {
+        console.log('button')
+        nav_home();
+    })
+    $('nav').on('click', '.lunch', function() {
+        nav_lunch();
+    })
+    $('nav').on('click', '.friends', function() {
+        nav_friends();
+    })
 })
