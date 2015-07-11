cd89cbe8730a19524a546bcdc0c3fd25db4418f2
mkidushim <mkidushim@gmail.com>
2015-07-11 11:06:49 -0700

    finished acount info page cannot edit yet #20

--- account.php
+++ account.php
@@ -4,13 +4,24 @@ require('mysql_connect.php');
 $id = $_SESSION['user_id'];
 $query = "SELECT * FROM `users` WHERE user_id = '$id'";
 $result = mysqli_query($con, $query);
+?>
+<h4 class="col-md-5 col-md-offset-2">Account Info</h4>
+<p onclick='logout()' id='logout' class='logout col-md-1 col-md-offset-1'>Logout</p>
+<p id='edit' class='logout col-md-1 col-md-offset-1'>Account</p>
+<nav class='col-xs-11 col-xs-offset-1 col-md-3 col-md-offset-9'>
+    <li class='home col-xs-3'>Home</li>
+    <li class='lunch col-xs-6 col-xs-offset-3'> Go to Lunch</li>
+</nav>
+
+<?php
+
 while ($row = mysqli_fetch_assoc($result)) {
 	$first_n = $row['first_name'];
 	$last_n = $row['last_name'];
 	$email = $row['email'];
 	$user = $row['username'];
 	$user_id= $row['user_id'];
-	echo "<h2>Username: ".$user." - ID Number:". $user_id."</h2><br><h2>Name: ".$first_n.$last_n."</h2><br/><h4>".$email."</h4>";
+	echo "<p class='col-md-12'>Username: ".$user."</p><br><p class='col-md-12'> ID Number:". $user_id."</p><br><p class='col-md-12'>Name: ".$first_n.$last_n."</p><br/><p class='col-md-12'>".$email."</p>";
 }
 
 ?>
