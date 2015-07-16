9dd731e0889d1c70d5ddebe320d0992c0f18977a
mkidushim <mkidushim@gmail.com>
2015-07-16 13:53:08 -0700

    #20 edit account page navigation matches rest of the site and works on mobile

--- account.php
+++ account.php
@@ -6,12 +6,16 @@ $query = "SELECT * FROM `users` WHERE user_id = '$id'";
 $result = mysqli_query($con, $query);
 ?>
 <h4 class="col-md-5 col-md-offset-2">Account Info</h4>
-<p onclick='logout()' id='logout' class='logout col-md-1 col-md-offset-1'>Logout</p>
-<p id='edit' class='logout col-md-1 col-md-offset-1'>Account</p>
-<nav class='col-xs-11 col-xs-offset-1 col-md-3 col-md-offset-9'>
-    <li class='home col-xs-3'>Home</li>
-    <li class='lunch col-xs-6 col-xs-offset-3'> Go to Lunch</li>
-</nav>
+<p onclick="login_check()" class='logout col-xs-3 col-md-1 col-md-offset-1'>Home</p>
+<p onclick="nav_lunch()" class='logout col-xs-3 col-md-1 col-md-offset-1'>Lunch</p>
+<p id="edit" onclick="nav_edit()" class="logout col-xs-3 col-md-1 col-md-offset-1">Account</p>
+
+<p id="logout" onclick="logout()" class="logout col-xs-3 col-md-1 col-md-offset-1">Logout</p>
+<!--<p onclick="nav_lunch()" class='lunch logout col-xs-6 col-md-1 col-md-offset-1'>Lunch</p>
+ <nav class='col-xs-11 col-xs-offset-1 col-md-3 col-md-offset-9'>
+    <li onclick="login_check()" class='home col-xs-3'>Home</li>
+    <li onclick="nav_lunch()" class='lunch col-xs-6 col-xs-offset-3'> Go to Lunch</li>
+</nav> -->
 
 <?php
 
@@ -21,7 +25,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 	$email = $row['email'];
 	$user = $row['username'];
 	$user_id= $row['user_id'];
-	echo "<p class='col-md-12'>Username: ".$user."</p><br><p class='col-md-12'> ID Number: ". $user_id."</p><br><p class='col-md-12'>First Name: ".$first_n."</p><br><p class='col-md-12'>Last Name: ".$last_n."</p><br/><p class='col-md-12'>E-mail: ".$email."</p>";
+	echo "<p class='col-xs-12 col-md-12'>Username: ".$user."</p><br><p class='col-xs-12 col-md-12'> ID Number: ". $user_id."</p><br><p class='col-xs-12 col-md-12'>First Name: ".$first_n."</p><br><p class='col-xs-12 col-md-12'>Last Name: ".$last_n."</p><br/><p class='col-xs-12 col-md-12'>E-mail: ".$email."</p>";
 	echo "<form> <input class='user col-md-7 col-md-offset-2' value='".$user."'></input><br><input class='name col-md-7 col-md-offset-2' value='".$first_n."'><input class='last col-md-7 col-md-offset-2' value='".$last_n."'></input><br/><input class='email col-md-7 col-md-offset-2' value='".$email."'></input>";
 }
 
