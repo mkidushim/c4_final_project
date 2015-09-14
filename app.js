f3025fb5842407473505598a5a025274efa0468c
mkidushim <mkidushim@gmail.com>
2015-09-13 19:37:06 -0700

    working on acccount creation validation #51

--- app.js
+++ app.js
@@ -1,43 +1,16 @@
-// var validationApp = angular.module('validationApp', []);
-var formApp = angular.module('formApp', []);
-
+var validationApp = angular.module('validationApp', []);
+var mkt;
 // create angular controller
-// validationApp.controller('mainController', function($scope) {
+validationApp.controller('mainController', function($scope) {
 
-//     // function to submit the form after all validation has occurred            
-//     $scope.submitForm = function(isValid) {
-
-//         // check to make sure the form is completely valid
-//         if (isValid) {
-//             alert('our form is amazing');
-//         }
-//     };
-
-// })
-
-
-function formController($scope, $http) {
-    $scope.formData = {};
-    $scope.processForm = function() {
-        $http({
-                method: 'POST',
-                url: 'validate.php',
-                data: $.param($scope.formData), // pass in data as strings
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                } // set the headers so angular passing info as form data (not request payload)
-            })
-            .success(function(data) {
-                console.log(data);
-
-                if (!data.success) {
-                    // if not successful, bind errors to error variables
-                    $scope.errorName = data.errors.name;
-                    $scope.errorSuperhero = data.errors.superheroAlias;
-                } else {
-                    // if successful, bind success message to message
-                    $scope.message = data.message;
-                }
-            });
+  // function to submit the form after all validation has occurred            
+  $scope.submitForm = function(isValid) {
+$scope.userForm = {};
+    // check to make sure the form is completely valid
+    if (isValid) {
+      alert(userForm.email);
     }
-}
+
+  };
+
+});
